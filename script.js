7b5b69f88650da1c5746d30992039751720175f7
Pidgicle <s_thomo@hotmail.co.uk>
2022-08-09 20:05:26 +0100

    Added functions 'playRound' and 'playGame' score counter on plaGame.

--- script.js
+++ script.js
@@ -6,7 +6,71 @@ Game will be run fully from the console,
 */
 
 const rpsChoices = ['Rock','Paper','Scissors'];
+let playerSelection;
 
 function getComputerChoice(){
     return rpsChoices[Math.floor(Math.random()*3)]
+}
+
+function playRound(playerSelection){
+    const formatChoice = playerSelection.toUpperCase();
+    const comChoice = getComputerChoice();
+    const tie = `It's a tie! You both chose ${playerSelection}!`;
+    const win = `You win! ${playerSelection} beats ${comChoice}`;
+    const lose = `You lose ${playerSelection} is beaten by ${comChoice}`
+    switch(formatChoice){
+        case 'ROCK':
+            switch(comChoice){
+                case 'Rock': console.log(tie);
+                    return 'draw'
+                case 'Paper': console.log(lose);
+                    return 'com';
+                case 'Scissors': console.log(win);
+                    return 'player';
+            }
+        case 'PAPER':
+            switch(comChoice){
+                case 'Rock': console.log(win);
+                    return 'player'
+                case 'Paper': console.log(tie);
+                    return 'draw';
+                case 'Scissors': console.log(lose);
+                    return 'com';
+            }
+        case 'SCISSORS':
+            switch(comChoice){
+                case 'Rock': console.log(lose);
+                    return 'com'
+                case 'Paper': console.log(win);
+                    return 'player';
+                case 'Scissors': console.log(tie);
+                    return 'draw';
+            }
+        default:
+            return 'Please enter Rock, Paper or Scissors'
+    }
+}
+
+function playGame(numberOfRounds = 5){
+    let playerWins = 0;
+    let comWins = 0;
+    let ties = 0;
+    for(let i = 0; i < numberOfRounds; i++){
+        switch(playRound(prompt())){
+            case 'com':
+                comWins++;
+                break;
+            case 'player':
+                playerWins++;
+                break;
+            case 'draw':
+                ties++;
+                break
+            default:
+                alert('Round did not count, please enter Rock, Paper or Scissors')
+                i--
+        }
+        console.log(`Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`)
+    }
+    console.log(`GAME OVER! Final Score: Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`)
 }
