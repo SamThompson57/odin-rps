5144a2f64258332074bead983b0955d169aa2ff9
Pidgicle <s_thomo@hotmail.co.uk>
2022-08-15 21:17:10 +0100

    Added UI to rock paper scissors, now can be selected by buttons

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Game will be run fully from the console,
 
 */
 
-const rpsChoices = ['Rock','Paper','Scissors'];
+const rpsChoices = ['ROCK','PAPER','SCISSORS'];
 let playerSelection;
 
 function getComputerChoice(){
@@ -13,47 +13,100 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection){
-    const formatChoice = playerSelection.toUpperCase();
+    console.log('The Game Has Started')
+    console.log(this)
+    const formatChoice = this.id.toUpperCase();
     const comChoice = getComputerChoice();
-    const tie = `It's a tie! You both chose ${playerSelection}!`;
-    const win = `You win! ${playerSelection} beats ${comChoice}`;
-    const lose = `You lose ${playerSelection} is beaten by ${comChoice}`
+    const tie = `It's a tie! You both chose ${formatChoice}!`;
+    const win = `You win! ${formatChoice} beats ${comChoice}`;
+    const lose = `You lose ${formatChoice} is beaten by ${comChoice}`
     switch(formatChoice){
         case 'ROCK':
             switch(comChoice){
-                case 'Rock': console.log(tie);
+                case 'ROCK': 
+                    results.textContent = tie;
                     return 'draw'
-                case 'Paper': console.log(lose);
+                case 'PAPER': 
+                    results.textContent = lose;
+                    scoreUpdate('com')
                     return 'com';
-                case 'Scissors': console.log(win);
+                case 'SCISSORS': 
+                    results.textContent = win;
+                    scoreUpdate('player')
                     return 'player';
             }
         case 'PAPER':
             switch(comChoice){
-                case 'Rock': console.log(win);
+                case 'ROCK': 
+                    results.textContent = win;;
+                    scoreUpdate('player')
                     return 'player'
-                case 'Paper': console.log(tie);
+                case 'PAPER': 
+                    results.textContent = tie;
                     return 'draw';
-                case 'Scissors': console.log(lose);
+                case 'SCISSORS': 
+                    results.textContent = lose;
+                    scoreUpdate('com')
                     return 'com';
             }
         case 'SCISSORS':
             switch(comChoice){
-                case 'Rock': console.log(lose);
+                case 'ROCK': 
+                    results.textContent = lose;
+                    scoreUpdate('com')
                     return 'com'
-                case 'Paper': console.log(win);
+                case 'PAPER': 
+                    results.textContent = win;
+                    scoreUpdate('player')
                     return 'player';
-                case 'Scissors': console.log(tie);
+                case 'SCISSORS': 
+                    results.textContent = tie;                    
                     return 'draw';
             }
         default:
-            return 'Please enter Rock, Paper or Scissors'
+            return 'Please enter Rock, Paper or Scissors';
     }
 }
 
-function playGame(numberOfRounds = 5){
-    let playerWins = 0;
-    let comWins = 0;
+const rock = document.querySelector('#Rock');
+rock.addEventListener('click', playRound);
+
+const paper = document.querySelector('#Paper');
+paper.addEventListener('click', playRound);
+
+const scissors = document.querySelector('#Scissors');
+scissors.addEventListener('click', playRound)
+
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+
+let playerWins = 0;
+let comWins = 0;
+
+function scoreUpdate(winner){
+    switch(winner){
+        case 'com':
+            comWins ++;
+            break;
+        
+        case 'player':
+            playerWins ++;
+            break;
+    }
+    score.textContent = `SCORE: Player: ${playerWins} CPU: ${comWins}`;
+    if(playerWins >= 5 || comWins >= 5 ){
+        alert(`GAME OVER! FINAL SCORE: Player: ${playerWins} CPU: ${comWins}`)
+        playerWins = 0;
+        comWins = 0;
+        score.textContent = '';
+    }
+
+
+}
+
+
+/*function playGame(numberOfRounds = 5){
+    
     let ties = 0;
     for(let i = 0; i < numberOfRounds; i++){
         switch(playRound(prompt())){
@@ -70,7 +123,7 @@ function playGame(numberOfRounds = 5){
                 alert('Round did not count, please enter Rock, Paper or Scissors')
                 i--
         }
-        console.log(`Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`)
+        console.log(`Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`);
     }
-    console.log(`GAME OVER! Final Score: Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`)
-}
+    console.log(`GAME OVER! Final Score: Player: ${playerWins} - Com: ${comWins} - Ties: ${ties}`);
+}*/
